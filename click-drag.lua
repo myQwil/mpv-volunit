b201c190c9e58fe8b114acd6081c033c26f2cfb3
Mike Will <myQwil@gmail.com>
2023-11-30 05:43:00 -0500

    add a click-drag script
    hold middle-click while moving the mouse to change the volume

---
+++ click-drag.lua
@@ -0,0 +1,38 @@
+local state = nil
+local o = {
+	gap = 20,    -- number of pixels between steps
+	step = 2,    -- step factor
+	dBmin = -60, -- silence threshold in decibels
+}
+(require 'mp.options').read_options(o)
+local gap   ,step   ,dBmin   ,dBmax
+=     o.gap ,o.step ,o.dBmin ,0
+local f = mp.get_property('ao') == 'pulse' and 60 or 20
+
+local function drag(_, pos)
+	local dif = math.floor((state.pos.y - pos.y) / gap + 0.5)
+	if state.dif ~= dif then
+		state.dif = dif
+		local dB = math.min(state.dB + (dif * step), dBmax)
+		local s = (dB <= dBmin and '-∞' or string.format('%+.0f', dB))..' dB'
+		mp.commandv('osd-bar', 'set', 'ao-volume', dB <= dBmin and 0 or 10 ^ (2 + dB / f))
+		mp.osd_message(string.format('AO-Volume: %s%s', s,
+			mp.get_property_bool('ao-mute') and ' (Muted)' or ''), 1)
+	end
+end
+
+local function click(t)
+	if t.event == 'down' then
+		local pos = mp.get_property_native('mouse-pos')
+		local vol = mp.get_property_number('ao-volume')
+		if not (pos and vol) then
+			return
+		end
+		state = { pos=pos, dif=0, dB=math.max(dBmin, f * math.log(vol / 100, 10)) }
+		mp.observe_property('mouse-pos', 'native', drag)
+	elseif t.event == 'up' then
+		mp.unobserve_property(drag)
+	end
+end
+
+mp.add_key_binding('mbtn_mid', click, {complex=true})
