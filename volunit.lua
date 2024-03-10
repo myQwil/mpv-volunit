943fa25434604099abd2355a0783edc3b270640c
Mike Will <myQwil@gmail.com>
2024-03-10 09:39:51 -0400

    Give the custom volume bar a simplified dark theme

--- volunit.lua
+++ volunit.lua
@@ -110,27 +110,36 @@ if o.custom_bar then
 		assdraw = require('mp.assdraw'),
 		osd = mp.create_osd_overlay('ass-events'),
 		color = {
-			font = '3aH00\\3cH000000\\1aH00\\1cHFFFFFF',
-			fill = '1aH00\\1cHA0FFFF',
-			back = '1aH80\\1cH400000',
-			pos  = '3aH00\\3cH00A0FF'
+			font = '1cH000000\\3cH808080',
+			back = '1aHD0\\1cHFFFFFF',
+			fill = '1cH000000',
+			pos  = '3cHFFFFFF',
+			max  = '3aH98\\3cHFFFF00',
+			line = '1aHFF\\3cH808080',
 		},
-		border = 3,
+		bord = 4,
 		win_w=0, win_h=0, w=0, h=0,
 		x=0, y=0, sx=0, sy=0,
 		sfpos=0, aopos=0,
 	}
-	b.half = b.border / 2
-	b.dent = b.border * 1.3
+	b.half = b.bord * 0.5
 
 	b.to_dimen = mp.add_timeout(0.15, function()
-		local ratio = b.osd.res_y / b.win_h
-		b.win_w, b.win_h = b.win_w * ratio, b.win_h * ratio
-		b.w, b.h = b.win_w * 0.75, b.win_h * 0.03125
-		b.x, b.y = (b.win_w - b.w) * 0.5, (b.win_h - b.h) * 0.75
-		b.sx, b.sy = b.x - b.h * 0.25, b.y + b.h * 0.5
-		b.sfpos = (b.w - b.border) * ((0 - dBmin) / (sfmax - dBmin)) + b.half
-		b.aopos = (b.w - b.border) * ((0 - dBmin) / (aomax - dBmin)) + b.half
+		b.win_w = b.win_w * (b.osd.res_y / b.win_h)
+		b.win_h = b.osd.res_y
+		b.w = b.win_w * 0.75
+		b.h = b.win_h * 0.03125
+		b.x = (b.win_w - b.w) * 0.5
+		b.y = (b.win_h - b.h) * 0.75
+
+		-- dent the bar at the 100% volume position
+		b.dent = b.h * 0.25
+		b.sfpos = (b.w - b.bord) * (0 - dBmin) / (sfmax - dBmin) + b.half
+		b.aopos = b.w - b.half
+
+		-- speaker symbol is anchored to this position
+		b.sx = b.x - b.h * 0.25
+		b.sy = b.y + b.h * 0.5
 	end, true)
 
 	mp.observe_property('osd-dimensions', 'native', function(_, win)
@@ -141,48 +150,37 @@ if o.custom_bar then
 		end
 	end)
 
+	function ass_draw(ass, fn, bord, color, x, y, w, h)
+		ass:append(string.format('\n{\\bord%g\\%s\\pos(%g,%g)}', bord, color, x, y))
+		ass:draw_start()
+		fn(ass, w, h)
+		ass:draw_stop()
+	end
+
+	function rect(ass, w, h)
+		ass:rect_cw(0, 0, w, h)
+	end
+
+	function marker(ass, pos, h)
+		ass:move_to(pos, 0)
+		ass:line_to(pos, h)
+	end
+
 	function b:draw_bar(dB, dBmax, ao)
 		local color = self.color
-		local border, half, dent = self.border, self.half, self.dent
+		local bord, half = self.bord, self.half
 		local x, y, w, h = self.x, self.y, self.w, self.h
-		local pos = (w - border) * ((math.max(dBmin, dB) - dBmin) / (dBmax - dBmin)) + half
+		local pos = (w - bord) * ((math.max(dBmin, dB) - dBmin) / (dBmax - dBmin)) + half
+		local pos0 = (ao == '') and self.sfpos or self.aopos
 		local draw = self.assdraw.ass_new()
 		draw:append(string.format('{\\an6\\bord%g\\%s\\pos(%g,%g)'
-			..'\\fnmpv-osd-symbols}', border, color.font, self.sx, self.sy))
+			..'\\fnmpv-osd-symbols}', bord, color.font, self.sx, self.sy))
 
-		draw:append(string.format('\n{\\bord0\\%s\\pos(%g,%g)}', color.back, x, y))
-		draw:draw_start() -- back area
-		draw:rect_cw(0, 0, w, h)
-		draw:draw_stop()
-
-		draw:append(string.format('\n{\\bord0\\%s\\pos(%g,%g)}', color.fill, x, y))
-		draw:draw_start() -- filled area
-		draw:rect_cw(0, 0, pos, h)
-		draw:draw_stop()
-
-		draw:append(string.format('\n{\\bord%g\\%s\\pos(%g,%g)}',
-			border, color.font, x, y))
-		draw:draw_start()
-		draw:rect_cw(-border, -border, w + border, h + border) -- the box
-		draw:rect_ccw(0, 0, w, h) -- the "hole"
-
-		if ao == '' then
-			local pos0 = self.sfpos
-			draw:move_to(pos0 + dent, 0)
-			draw:line_to(pos0, dent)
-			draw:line_to(pos0 - dent, 0)
-			draw:move_to(pos0 - dent, h)
-			draw:line_to(pos0, h - dent)
-			draw:line_to(pos0 + dent, h)
-			draw:draw_stop()
-		end
-
-		draw:append(string.format('\n{\\bord%g\\%s\\pos(%g,%g)}',
-			half, color.pos, x, y))
-		draw:draw_start() -- position marker
-		draw:move_to(pos, 0)
-		draw:line_to(pos, h)
-		draw:draw_stop()
+		ass_draw(draw, rect, 0, color.back, x, y, w, h)        -- back area
+		ass_draw(draw, rect, 0, color.fill, x, y, pos, h)      -- filled area
+		ass_draw(draw, marker, half, color.pos, x, y, pos, h)  -- position marker
+		ass_draw(draw, marker, half, color.max, x, y, pos0, h) -- 100% marker
+		ass_draw(draw, rect, bord, color.line, x, y, w, h)     -- outline
 
 		self.osd.data = draw.text
 		self.osd:update()
